6db13c7f54905c05f57c3384d393de83010dfd7b
Pablo Ortega Mesa <portega@modyo.com>
2018-03-07 10:02:41 -0300

    Adding dog model

---
+++ dogs/db/migrate/20180307130112_create_dogs.rb
@@ -0,0 +1,16 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.string :sound
+      t.string :meme_phrase
+      t.string :age
+      t.string :gender
+      t.string :coat_length
+      t.string :size
+
+      t.timestamps
+    end
+  end
+end
