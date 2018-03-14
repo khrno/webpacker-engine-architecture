570c8caf83c651d2e3e4b416b6e4f58db7af13f9
Pablo Ortega Mesa <portega@modyo.com>
2018-03-14 16:15:28 -0300

    Avoid duplication of package.json yarn.lock and node_modules

--- sandbox/config/webpack/environment.js
+++ sandbox/config/webpack/environment.js
@@ -1,6 +1,11 @@
 const path = require('path')
 const { environment } = require('@rails/webpacker')
 
+// Lookup for node_modules only in sandbox/node_modules, so isn't necessary have those folders in the meta-engine
+environment.config.set('resolve.modules', [
+  path.resolve('node_modules')
+])
+
 environment.config.set('resolve.alias', {
   Cats: path.resolve('..', 'cats/app/javascript/components'),
   Dogs: path.resolve('..', 'dogs/app/javascript/components')
